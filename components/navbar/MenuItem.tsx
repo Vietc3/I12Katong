d32348a91afe4f1cb38eea7898db2aee875e0708
QuocViet <vietdat657@gmail.com>
2021-04-26 21:25:59 +0700

    fix color

--- components/navbar/MenuItem.tsx
+++ components/navbar/MenuItem.tsx
@@ -4,15 +4,16 @@ import { BoxProps, Button, Text } from '@chakra-ui/react';
 
 interface Props extends BoxProps {
     to: string;
-    children?:any
+    isLast?: boolean;
+    children?:any;
 }
 
-const MenuItem: React.FC<Props> = ({ children, to}) => {
+const MenuItem: React.FC<Props> = ({ children, isLast = false, to, ...rest }) => {
     return (
         <Link  href={to}>
-        <Button borderRadius={0} _hover={{background:"#fffff"}} _focus={{borderBottom:"1.5px solid #A68340", borderTop:"0px", borderLeft:"0px", borderRight:"0px"}}  variant={'ghost'}  >
+        <Button borderRadius={0} _hover={{background:"#fffff"}} _focus={{borderBottom:"1.5px solid #A68340", borderTop:"0px", borderLeft:"0px", borderRight:"0px"}}  variant={'ghost'}  {...rest}>
           
-                <a >
+                <a>
                     <Text fontWeight="bold"  display="block">{children}</Text>
                 </a>
           
