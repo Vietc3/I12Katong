a0d20e39c2ad1d5f217881ff45a92c37111f7546
AP-MEDIA\VNguye21 <vietquoc.nguyen@isobar.com>
2021-05-07 10:23:48 +0700

    update

--- constants/index.ts
+++ constants/index.ts
@@ -8,10 +8,6 @@ export const NAVBAR_LINKS = [
         name: 'STORES',
     },
     {
-        to: '/articles',
-        name: 'ARTWORKS',
-    },
-    {
         to: '/deals',
         name: 'DEALS',
     },
